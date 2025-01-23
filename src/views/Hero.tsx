44b360feb0705b818cc8c8c4fba0ecfbb1763742
jsaligumba-ghub <jsaligumba1336qc@student.fatima.edu.ph>
2025-01-24 02:47:04 +0800

    f

---
+++ src/views/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <section className="w-full h-screen bg-black">
+        <div className="w-full min-h-screen flex">
+            <h1 className='text-3xl'>Testing</h1>
+        </div>
+    </section>
+  )
+}
